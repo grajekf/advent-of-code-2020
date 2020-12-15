10473d2d4543e6b71d7ed59a95bd6e5a686d69bd
Filip Grajek <filip.grajek@gmail.com>
2020-12-15 21:13:50 +0100

    SOlution to 15a

---
+++ 15a/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _15a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 9, 3, 1, 0, 8, 4 };
+            // List<int> numbers = new List<int> { 0, 3, 6 };
+
+            for (int i = numbers.Count; i <= 2019; i++)
+            {
+                var lastNumber = numbers.Last();
+                var index = numbers.Take(numbers.Count - 1).ToList().LastIndexOf(lastNumber);
+                if (index == -1)
+                {
+                    numbers.Add(0);
+                }
+                else
+                {
+                    numbers.Add(i - index - 1);
+                }
+            }
+
+            Console.WriteLine(numbers.Last());
+        }
+    }
+}
