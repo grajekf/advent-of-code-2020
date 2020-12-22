39a5c56486fc896509336cbe4d355ea3d3b89e90
Filip Grajek <filip.grajek@gmail.com>
2020-12-22 13:14:21 +0100

    Equals and GetHashcode for state

--- 22b/Program.cs
+++ 22b/Program.cs
@@ -17,15 +17,16 @@ namespace _22a
                 state = state.Step();
             }
 
-            Console.WriteLine(state.GetScore());
+            Console.WriteLine(state.GetScore(Player.One));
+            Console.WriteLine(state.GetScore(Player.Two));
         }
 
     }
 
-    class GameState
+    struct GameState
     {
-        private IList<int> _playerOneDeck;
-        private IList<int> _playerTwoDeck;
+        private IEnumerable<int> _playerOneDeck;
+        private IEnumerable<int> _playerTwoDeck;
 
         public GameState(IEnumerable<int> playerOneDeck, IEnumerable<int> playerTwoDeck)
         {
@@ -33,16 +34,16 @@ namespace _22a
             _playerTwoDeck = playerTwoDeck.ToList();
         }
 
-        public bool IsGameFinished => _playerOneDeck.Count == 0 || _playerTwoDeck.Count == 0;
-        public int GetScore()
+        public bool IsGameFinished => _playerOneDeck.Count() == 0 || _playerTwoDeck.Count() == 0;
+        public int GetScore(Player player)
         {
-            var deckToCheck = _playerOneDeck.Count == 0 ? _playerTwoDeck : _playerOneDeck;
+            var deckToCheck = player == Player.Two ? _playerTwoDeck : _playerOneDeck;
             var score = 0;
 
-            for (int i = deckToCheck.Count - 1; i >= 0; i--)
+            for (int i = deckToCheck.Count() - 1; i >= 0; i--)
             {
-                var multiplier = deckToCheck.Count - i;
-                score += deckToCheck[i] * multiplier;
+                var multiplier = deckToCheck.Count() - i;
+                score += deckToCheck.ElementAt(i) * multiplier;
             }
 
             return score;
@@ -82,6 +83,41 @@ namespace _22a
             var playerTwoCards = playerTwoDeckString.Split("\r\n").Skip(1).Select(int.Parse);
 
             return new GameState(playerOneCards, playerTwoCards);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is GameState state &&
+                   _playerOneDeck.SequenceEqual(state._playerOneDeck) &&
+                   _playerTwoDeck.SequenceEqual(state._playerTwoDeck);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_playerOneDeck.HashCodeFromContents(), _playerTwoDeck.HashCodeFromContents());
+        }
+
+        public static bool operator ==(GameState x, GameState y) => x.Equals(y);
+        public static bool operator !=(GameState x, GameState y) => !(x == y);
+    }
+
+    enum Player
+    {
+        One,
+        Two
+    }
+
+    static class IEnumerableExtensions
+    {
+        public static int HashCodeFromContents<T>(this IEnumerable<T> seq)
+        {
+            var result = 17;
+            foreach (var item in seq)
+            {
+                result = (result * 23) + item.GetHashCode();
+            }
+
+            return result;
         }
     }
 }
