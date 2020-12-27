9bce72f32b4250095cec1c694d26f0ea652a7191
Filip Grajek <filip.grajek@gmail.com>
2020-12-27 13:28:29 +0100

    Solution for 2a

---
+++ 02a/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _02a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+
+            var passwords = lines.Select(l => PasswordWithRule.Parse(l)).ToList();
+            Console.WriteLine(passwords.Count(p => p.IsValid()));
+        }
+    }
+
+    class PasswordWithRule
+    {
+        public PasswordWithRule(int from, int to, char character, string password)
+        {
+            From = from;
+            To = to;
+            Character = character;
+            Password = password;
+        }
+
+        public int From { get; set; }
+        public int To { get; set; }
+        public char Character { get; set; }
+        public string Password { get; set; }
+
+        public static PasswordWithRule Parse(string input)
+        {
+            var parts = input.Split(" ");
+            var fromToPart = parts[0];
+            var charPart = parts[1];
+            var passwordPart = parts[2];
+
+            var fromToSplit = fromToPart.Split("-");
+            var from = int.Parse(fromToSplit[0]);
+            var to = int.Parse(fromToSplit[1]);
+
+            var character = charPart[0];
+
+            return new PasswordWithRule(from, to, character, passwordPart);
+        }
+
+        public bool IsValid()
+        {
+            var charCount = Password.Count(c => c == Character);
+            return charCount >= From && charCount <= To;
+        }
+    }
+}
