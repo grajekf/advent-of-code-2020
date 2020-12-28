84c58837cb89c6d2e272c18e53f5e8ce6a06a66f
Filip Grajek <filip.grajek@gmail.com>
2020-12-28 23:48:53 +0100

    Base for 04

---
+++ 04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var passports = File.ReadAllText("input.txt")
+                .Split("\r\n\r\n")
+                .Select(p => p.Replace("\r\n", " "))
+                .ToList();
+
+        }
+    }
+}
