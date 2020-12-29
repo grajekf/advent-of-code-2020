525b35b46c8c6900ad2afb42c5a3f05fe83d5eb4
Filip Grajek <filip.grajek@gmail.com>
2020-12-29 13:58:58 +0100

    Solution for 05a

---
+++ 05/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var seats = File.ReadAllLines("input.txt");
+            Console.WriteLine(seats.Max(s => CalculateId(s)));
+        }
+
+        static int CalculateId(string seat)
+        {
+            var row = ToDecimal(seat.Substring(0, 7), 'F', 'B');
+            var col = ToDecimal(seat.Substring(7), 'L', 'R');
+
+            return row * 8 + col;
+        }
+
+        static int ToDecimal(string input, char zero, char one)
+        {
+            input = input.Replace(zero, '0');
+            input = input.Replace(one, '1');
+
+            return Convert.ToInt32(input, 2);
+        }
+    }
+}
