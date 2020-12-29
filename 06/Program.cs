f3ac4b08326d1f71ecea92236eb6c1bdcad0cf68
Filip Grajek <filip.grajek@gmail.com>
2020-12-29 14:11:08 +0100

    Solution for 06b

--- 06/Program.cs
+++ 06/Program.cs
@@ -10,7 +10,10 @@ namespace _06
         static void Main(string[] args)
         {
             var groups = File.ReadAllText("input.txt").Split("\r\n\r\n").Select(g => g.Split("\r\n"));
-            Console.WriteLine(groups.Sum(g => g.Aggregate(string.Empty.Select(c => c), (u, answers) => u.Union(answers), r => r.Count())));
+            Console.WriteLine(groups
+                .Sum(g => g.Aggregate(string.Empty.Select(c => c), (u, answers) => u.Union(answers), r => r.Count())));
+            Console.WriteLine(groups
+              .Sum(g => g.Aggregate(alphabet.Select(c => c), (u, answers) => u.Intersect(answers), r => r.Count())));
         }
     }
 }
