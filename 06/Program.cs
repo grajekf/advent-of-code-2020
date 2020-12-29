3fd0cf8fb4347ff6d7fe809f024dcb73a1370824
Filip Grajek <filip.grajek@gmail.com>
2020-12-29 14:10:18 +0100

    Solution for 06a

---
+++ 06/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _06
+{
+    class Program
+    {
+        const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        static void Main(string[] args)
+        {
+            var groups = File.ReadAllText("input.txt").Split("\r\n\r\n").Select(g => g.Split("\r\n"));
+            Console.WriteLine(groups.Sum(g => g.Aggregate(string.Empty.Select(c => c), (u, answers) => u.Union(answers), r => r.Count())));
+        }
+    }
+}
